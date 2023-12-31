eee4918cce58c145cc82c461e248fc0a0cc0def4
newtechnologylifestyle <mats01102@gmail.com>
2023-12-31 18:16:26 +0900

    chap07_sec04

--- photo/urls.py
+++ photo/urls.py
@@ -8,4 +8,12 @@ app_name = 'photo'
 urlpatterns = [
     # photoアプリへのアクセスはviewsモジュールのIndexViewを実行
     path('', views.IndexView.as_view(), name='index'),
+    
+    # 写真投稿ページへのアクセスはviewsモジュールのCreatePhotoViewを実行
+    path('post/', views.CreatePhotoView.as_view(), name='post'),
+    
+    # 投稿完了ページへのアクセスはviewsモジュールのPostSuccessViewを実行
+    path('post_done/',
+         views.PostSuccessView.as_view(),
+         name='post_done'),
 ]
